b99b7dde9be5583892c3fe262ece5d90bd58491e
LiYingWang <jaybin502@gmail.com>
2023-01-13 08:39:44 -0800

    worked on the site map

--- analysis/code/site-regional-map-Ch.R
+++ analysis/code/site-regional-map-Ch.R
@@ -17,18 +17,18 @@ world_points <- sf::st_point_on_surface(world)
 world_points <- cbind(world, st_coordinates(st_point_on_surface(world$geometry))) %>%
   filter(brk_name %in% c("Thailand", "China", "Myanmar",
                          "Laos", "Vietnam", "Cambodia")) %>%
-  mutate(Y = Y+4, X = X-2)
+  mutate(Y = Y+2, X = X-1)
 
 # add site location
 site_location <-
-  data.frame(location = c("Guijuabao", "Gaoshan", "Chengdu"),
-             lon = c(101.3617, 103.3446, 104.0587),
-             lat = c(27.2657, 30.2709, 30.5899))
+  data.frame(location = c("Guijuabao", "Gaoshan"), # Chengdu(104.0587, 30.5899)
+             lon = c(101.3617, 103.3446),
+             lat = c(27.2657, 30.2709))
 
 China_SE_Asia <-
   ggplot(data = world) +
   geom_sf( fill= "antiquewhite") +
-  geom_rect(xmin = 100.5, xmax = 102.5, ymin = 26.5, ymax = 28.5,
+  geom_rect(xmin = 100, xmax = 106, ymin = 26, ymax = 32,
             fill = NA, colour = "red", size = 0.5) +
   geom_shadowtext(data= world_points,
                   aes(x = X, y = Y,
@@ -39,9 +39,9 @@ China_SE_Asia <-
                   position = position_nudge(y = - 1.7, x = 0.5)) +
   #annotate(geom = "text", x = 102.3, y = 9.4, label = "Gulf of\nTailand",
            #fontface = "italic", color = "grey22", size = 2) +
-  coord_sf(xlim = c(85, 115), ylim = c(16, 42), expand = FALSE) + #add datum = NA to remove
-  scale_x_continuous(breaks = seq(80, 120, by = 10)) +
-  scale_y_continuous(breaks = seq(10, 50, by = 10)) +
+  coord_sf(xlim = c(80, 123), ylim = c(9, 45), expand = FALSE) + #add datum = NA to remove
+  scale_x_continuous(breaks = seq(80, 125, by = 10)) +
+  scale_y_continuous(breaks = seq(5, 45, by = 10)) +
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank())
 
@@ -49,8 +49,8 @@ China_SE_Asia <-
 library(ggmap)
 # we don't want to download every time, so let's save the map locally
 # from https://stackoverflow.com/a/52710855/1036500
-GJB_map <- ggmap(get_stamenmap(rbind(as.numeric(c(100.5, 26.5,
-                                                  104.5, 30.5))), zoom = 10))
+GJB_map <- ggmap(get_stamenmap(rbind(as.numeric(c(100, 26,
+                                                  106, 32))), zoom = 10))
 # saveRDS(tw_map, here("analysis", "data", "raw_data", "tw_map.rds"))
 # tw_map <- readRDS(here("analysis", "data", "raw_data", "tw_map.rds"))
 pg <- ggplot_build(GJB_map)
@@ -60,7 +60,7 @@ China_map_with_site <-
   geom_point(data = site_location,
              aes(x = lon,
                  y = lat),
-             size = 2,
+             size = 1,
              color = "red") +
   geom_shadowtext(data = site_location,
                   aes(x = lon,
@@ -68,13 +68,13 @@ China_map_with_site <-
                       label = location),
                   color='black',
                   bg.colour='white',
-                  size = 3,
-                  position = position_nudge(y = 0.11),
+                  size = 2.6,
+                  position = position_nudge(y = - 0.2),
                   check.overlap = TRUE) +
-  #annotate(geom = "text", x = 103.95, y = 13.0, label = "Tonle Sap\nLake",
-           #fontface = "italic", color = "blue", size = 2) +
-  coord_sf(xlim = c(100.5, 104.5),
-           ylim = c(26.5, 30.5),
+  annotate(geom = "text", x = 104, y = 30.7, label = "Chengdu\nPlain",
+           fontface = "italic", color = "grey15", size = 2) +
+  coord_sf(xlim = c(100, 106),
+           ylim = c(26, 32),
            expand = FALSE) +
   #scale_x_continuous(breaks = c(121.0, 121.5, 122.0),
   #limits = c(120.9, 122.7)) +
@@ -82,25 +82,17 @@ China_map_with_site <-
     # edit these numbers to select a suitable location
     # for the scale bar where it does not cover
     # important details on the map
-    lon = 100.6,
-    lat = 26.6,
-    legend_size = 2,
-    # distance of one section of scale bar, in km
-    distance_lon = 20,
-    # height of the scale bar, in km
-    distance_lat = 1,
-    # distance between scale bar and units, in km
-    distance_legend = 5,
-    # units of scale bar
-    dist_unit = "km",
-    # add the north arrow
-    orientation = TRUE,
-    # length of N arrow, in km
-    arrow_length = 6,
-    # distance between scale bar & base of N arrow, in km
-    arrow_distance = 11,
-    # size of letter 'N' on N arrow, in km
-    arrow_north_size = 4) +
+    lon = 104.6,
+    lat = 26.2,
+    legend_size = 1.6, # size of scale legend
+    distance_lon = 50, # distance of one section of scale bar, in km
+    distance_lat = 1.8, # height of the scale bar, in km
+    distance_legend = 17, # distance between scale bar and units, in km
+    dist_unit = "km", # units of scale bar
+    orientation = TRUE, # add the north arrow
+    arrow_length = 10, # length of N arrow, in km
+    arrow_distance = 35, # distance between scale bar & base of N arrow, in km
+    arrow_north_size = 2.5) + # size of letter 'N' on N arrow, in km
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank())
 
@@ -121,7 +113,7 @@ ggplot() +
   theme_void() +
   theme(plot.background = element_rect(color = "white", fill = "white"))
 
-ggsave(here::here("analysis","figures", "Chengdu-sites-map.png"),
+ggsave(here::here("analysis","figures", "Sichuan-sites-map.png"),
        width = 6,
        height = 3,
        dpi = 300,
